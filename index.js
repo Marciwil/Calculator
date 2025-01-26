24cf401265127f20b74aabb01385c5f094c25eff
Marcie Williams <190632913+Marciwil@users.noreply.github.com>
2025-01-26 11:49:13 -0500

    Created functions for the operators and execution button" "

--- index.js
+++ index.js
@@ -21,47 +21,86 @@
 let num1 // starting value numbers
 let num2
 let operator= "";// requires an operator to be entered in
+let previousButtonPressed
 
 function add(a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 }
 
-function subtract(a, b) {
-    return a - b;
+function sub(a, b) {
+    return Number(a) - Number(b);
 }
 
 function multiply(a, b) {
-    return a * b;
+    return Number(a) * Number(b);
 }
 
 function divide(a, b) {
-    return a / b;
+    return Number(a) / Number(b);
 }
 
-function operate(num1 , num2) {
+function operate() {
+    if(num1 && !operator) {
+        updateDisplay(num1)
+    }
+    if(!num1 || !operator) {
+        console.log('here')
+        return 
+    }
+    if(!num2) {
+        console.log('display',display.textContent)
+        num2 = display.textContent
+    } 
     let result
-    if(operator === '+'){
+    if(operator === '+') {
         result = add(num1, num2)
     }
-    
-    return result
-}
+    if(operator  === "-") {
+        result = sub(num1, num2)
+    }
+    if(operator === 'x') {
+        result = multiply(num1, num2)
+    }
+    if(operator === '/') {
+        result = divide(num1, num2)
+    }
+    num1= ""
+    num2= ""
+    operator = ""
+    updateDisplay(result)
+};
 
 const display= document.querySelector(".display");
 
 function updateDisplay(input){
     console.log(input)
-    display.textContent= display.textContent + input // take present content and add more to it.
+    if(previousButtonPressed === "operator"){
+        return
+    }
+    if(previousButtonPressed!="operator" && previousButtonPressed != 'equals'){
+        display.textContent= display.textContent + input // take present content and add more to it.
+    } else {
+        display.textContent= input
+    }
 };
 
 function clearDisplay(){
     display.textContent= ""
-}
+};
 
 const keys = document.querySelectorAll('.number')
 keys.forEach((button) => {
     button.addEventListener('click', () => {
+        previousButtonPressed= button.value
         updateDisplay(button.value) 
+    });
+});
+
+const operatorKeys = document.querySelectorAll('.operator')
+operatorKeys.forEach((button) => {
+    button.addEventListener('click', () => {
+        previousButtonPressed= 'operator'
+        updateDisplay(button.value)
     });
 });
 
@@ -70,5 +109,51 @@ clear.addEventListener('click', () => {
     clearDisplay()
 });
 
-const addition= document.getElementById("add")
-addition.addEventListener('click', add)
+const addition= document.querySelector("#add")
+addition.addEventListener('click', () => {
+    operator= "+"
+    previousButtonPressed= "operator"
+    if (!num1){
+        num1 = display.textContent
+    }
+
+    clearDisplay();
+    })
+    
+
+const subtract= document.querySelector("#subtract")
+subtract.addEventListener('click', () => {
+    operator= "-"
+    previousButtonPressed= "operator"
+   
+    clearDisplay()
+    });
+
+const product= document.querySelector("#multiply")
+product.addEventListener('click', () => {
+    operator= '*'
+    operate(operator);
+    previousButtonPressed= "operator"
+    clearDisplay()
+});
+
+const division= document.querySelector("#divide")
+division.addEventListener('click', () => {
+    operator= "/"
+    previousButtonPressed= "operator"
+    clearDisplay()
+});
+
+const allClear=document.querySelector("#allclear")
+allClear.addEventListener('click', () => {
+    clearDisplay() 
+    num1= ""
+    num2= ""
+    operator = ""
+});
+
+const equal=document.querySelector("#exe")
+equal.addEventListener('click', () => {
+    previousButtonPressed= "equals"
+    operate()
+});
